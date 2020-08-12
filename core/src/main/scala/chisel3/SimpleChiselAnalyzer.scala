ff3bbfb029dcb441cc7eff31e11c8074b68c33d2
Shibo <chshibo@umich.edu>
2020-08-12 14:57:33 -0400

    Added APIs

--- core/src/main/scala/chisel3/SimpleChiselAnalyzer.scala
+++ core/src/main/scala/chisel3/SimpleChiselAnalyzer.scala
@@ -46,4 +46,42 @@ object SimpleChiselAnalyzer{
         }
         return None
     }
+
+    def findUses(id: Arg, ctx: ArrayBuffer[Command]): Option[Seq[Command]] = {
+        //TODO: Implement this
+        return None
+    }
+
+    def findSameCycleDependencies(id: Arg, ctx: ArrayBuffer[Command]): Option[Seq[Arg]] = {
+        //TODO: Implement this
+        return None
+    }
+
+    def findDependencies(id: Arg, ctx: ArrayBuffer[Command]): Option[Seq[Arg]] = {
+        //TODO: Implement this
+        return None
+    }   
+
+    // Whether lId is dependent on rId
+    def hasSameCycleDependency(lId: Arg, rId: Arg, ctx: ArrayBuffer[Command]): Boolean  = {
+        //TODO: Implement this
+        return false
+    } 
+
+    // Whether lId is dependent on rId
+    def hasDependency(lId: Arg, rId: Arg, ctx: ArrayBuffer[Command]): Boolean = {
+        //TODO: Implement this
+        return false
+    }
+
+    def findIndirectDependencies(id: Arg, ctx: ArrayBuffer[Command]): Option[Seq[Command]] = {
+        //TODO: Implement this
+        return None
+    }
+
+    def findDirectDependencies(id: Arg, ctx: ArrayBuffer[Command]): Option[Seq[Command]] = {
+        //TODO: Implement this
+        return None
+    }
+
 }
--- core/src/main/scala/chisel3/SimpleChiselTransformer.scala
+++ core/src/main/scala/chisel3/SimpleChiselTransformer.scala
@@ -6,5 +6,28 @@ import chisel3.internal.firrtl._
 import chisel3.experimental._
 
 object SimpleChiselTranformer{
-    
+    //replace lId with rId in ctx and return the new ctx
+    def replace(lId: Arg, rId: Arg, ctx: ArrayBuffer[Command]): Option[rrayBuffer[Command]] ={
+        //TODO
+        return replaceWhen(lId, rId, ctx)(true)
+    }
+
+    //replace lId with rId in ctx and return the new ctx
+    def replaceWhen(lId: Arg, rId: Arg, ctx: ArrayBuffer[Command])(pred: () => Boolean): Option[ArrayBuffer[Command]] ={
+        //TODO
+        return None
+    }
+
+    def removeIfSafe(id: Arg, ctx: ArrayBuffer[Command]): Option[ArrayBuffer[Command]] ={
+        //TODO
+        return None   
+    }
+
+    // find the most front place to insert the cmd
+    def Insert(cmd: Command, ctx: ArrayBuffer[Command]): Option[ArrayBuffer[Command]] ={
+        //TODO
+        return None   
+    }
+
+    // Update functions (Optional)
 }
